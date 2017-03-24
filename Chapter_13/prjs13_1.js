19feaca3c0a7f018de3d6b1796cd2908d9dddd5d
Falieson <Falieson@users.noreply.github.com>
2017-03-24 15:52:50 -0700

    Update prjs13_1.js

--- Chapter_13/prjs13_1.js
+++ Chapter_13/prjs13_1.js
@@ -1,8 +1,12 @@
-var getEmail = document.getElementById('getemail')
-getEmail.addEventListener('submit', function (event) {
-  var emailAdd = document.getElementById('email').value
-  event.preventDefault()
-  if ((emailAdd.indexOf('@') >= 0) && (emailAdd.indexOf('.') >= 0)) {
-    window.alert('Thank you!')
-  } else { window.alert('The @ and . characters are required in the e-mail address.') }
-})
+window.onload = function() { // this will be run when the whole page is loaded
+  var getEmail = document.getElementById('emailForm');
+  getEmail.addEventListener('submit', function (event) {
+    event.preventDefault()
+    var emailAdd = document.getElementById('email').value
+    if ((emailAdd.indexOf('@') >= 0) && (emailAdd.indexOf('.') >= 0)) {
+      window.alert('Thank you!')
+    } else {
+      window.alert('The @ and . characters are required in the e-mail address.')
+    }
+  })
+};
